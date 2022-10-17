62979baed3dbe05fcb6afe66c6b69f7b329d8004
BHM-Bob <2262029386@qq.com>
2022-10-18 00:41:35 +0800

    上一个提交删除了BA_Array，update MyBA_Ver

--- BA_1_2/BA_1_2.cpp
+++ BA_1_2/BA_1_2.cpp
@@ -18,7 +18,7 @@ MyBA* pba;
 
 float MyBA_Ver(void)
 {
-	return 1.3820;
+	return 1.3920;
 	/*
 	* 1.1000 : 1.1.0.0 :2020年08月15日：MyUI, MyDir(in C)
 	* 1.2000 : 1.2.0.0 :2021年03月28日：MyBA, List
@@ -41,6 +41,7 @@ float MyBA_Ver(void)
 	* 1.3621 : 2.7.2.1 :2022年08月07日: 将Que和balist合并；修复BA_String::Splitx bug；修改部分注释
 	* 1.3750 : 2.8.5.0 :2022年10月07日: Console分支：删除GUI，删除部分无用组件代码，部分添加namespace，添加开源项目nlohmann/json
 	* 1.3820 : 2.9.2.0 :2022年10月09日: Console分支：添加开源项目cppjieba, 添加字符编码处理能力
+	* 1.3920 : 2.10.2.0:2022年10月18日: Console分支：添加开源项目glob, 添加ba::tensor, 删除BA_Array
 	*/
 }
 
